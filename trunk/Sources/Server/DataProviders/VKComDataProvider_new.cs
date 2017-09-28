a1fa47914e82b33d0f6ddaa7d421df16150660a9
d.kerner <denkern@gmail.com>
2017-09-28 10:09:33 +0700

    new reveal script

--- trunk/Sources/Server/DataProviders/VKComDataProvider_new.cs
+++ trunk/Sources/Server/DataProviders/VKComDataProvider_new.cs
@@ -290,59 +290,73 @@ namespace Jukebox.Server.DataProviders
 
         private String getRevealScript()
         {
-            return @"var res = function(t, i) { 
-                  ""use strict""; 
+            return @"var res = function(t, e) {
+    ""use strict"";
 
-                  function e(t) {
-                   if (~t.indexOf(""audio_api_unavailable"")) {
-                    var i = t.split(""?extra="")[1].split(""#""),
-                     e = o(i[1]);
-                    if (i = o(i[0]), !e || !i) return t;
-                    e = e.split(String.fromCharCode(9));
-                    for (var a, r, l = e.length; l--;) {
-                     if (r = e[l].split(String.fromCharCode(11)), a = r.splice(0, 1, i)[0], !s[a]) return t;
-                     i = s[a].apply(null, r)
-                    }
-                    if (i && ""http"" === i.substr(0, 4)) return i
-                   }
-                   return t
-                  }
+    function i() {
+        return false; 
+    }
 
-                  function o(t) {
-                   if (!t || t.length % 4 == 1) return !1;
-                   for (var i, e, o = 0, s = 0, r = """"; e = t.charAt(s++);){ 
-                    e = a.indexOf(e), ~e && 
-                    (i = o % 4 ? 64 * i + e : e, o++ % 4) && (r += String.fromCharCode(255 & i >> (-2 * o & 6)));
+    function o(t) {
+        if (~t.indexOf(""audio_api_unavailable"")) {
+            var e = t.split(""?extra="")[1].split(""#""),
+                o = """" === e[1] ? """" : a(e[1]);
+            if (e = a(e[0]), ""string"" != typeof o || !e) return t;
+            o = o ? o.split(String.fromCharCode(9)) : [];
+            for (var s, r, n = o.length; n--;) {
+                if (r = o[n].split(String.fromCharCode(11)), s = r.splice(0, 1, e)[0], !l[s]) return t;
+                e = l[s].apply(null, r)
+            }
+            if (e && ""http"" === e.substr(0, 4)) return e
+        }
+        return t
+    }
 
-                   }
-                   return r
-                  }
-  
-                  var a = ""abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMN0PQRSTUVWXYZO123456789+/="",
-                   s = {
-                    v: function(t) {
-                     return t.split("""").reverse().join("""")
-                    },
-                    r: function(t, i) {
-                     t = t.split("""");
-                     for (var e, o = a + a, s = t.length; s--;) e = o.indexOf(t[s]), ~e && (t[s] = o.substr(e - i, 1));
-                     return t.join("""")
-                    },
-                    x: function(t, i) {
-                     var e = [];
-                     i = i.charCodeAt(0);
-                     var temp_arr = t.split("""");
-                     for (var temp_idx=0;temp_idx < temp_arr.length; temp_idx++) {
-                      var elem = temp_arr[temp_idx];
-                      e.push(String.fromCharCode(elem.charCodeAt(0) ^ i));
-                     }
-                     return e.join("""");
-                    }
-                  }
+    function a(t) {
+        if (!t || t.length % 4 == 1) return !1;
+        for (var e, i, o = 0, a = 0, s = """"; i = t.charAt(a++);) i = r.indexOf(i), ~i && (e = o % 4 ? 64 * e + i : i, o++ % 4) && (s += String.fromCharCode(255 & e >> (-2 * o & 6)));
+        return s
+    }
 
-                  return e;
+    function s(t, e) {
+        var i = t.length,
+            o = [];
+        if (i) {
+            var a = i;
+            for (e = Math.abs(e); a--;) o[a] = (e += e * (a + i) / e) % i | 0
+        
+	}        return o
+    }
+    var r = ""abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMN0PQRSTUVWXYZO123456789+/="",
+        l = {
+            v: function(t) {
+                return t.split("""").reverse().join("""")
+            },
+            r: function(t, e) {
+                t = t.split("""");
+                for (var i, o = r + r, a = t.length; a--;) i = o.indexOf(t[a]), ~i && (t[a] = o.substr(i - e, 1));
+                return t.join("""")
+            },
+            s: function(t, e) {
+                var i = t.length;
+                if (i) {
+                    var o = s(t, e),
+                        a = 0;
+                    for (t = t.split(""""); ++a < i;) t[a] = t.splice(o[i - 1 - a], 1, t[a])[0];
+                    t = t.join("""")
                 }
-                var reveal = res();";
+                return t
+            },
+            x: function(t, e) {
+                var i = [];
+                return e = e.charCodeAt(0), each(t.split(""""), function(t, o) {
+                    i.push(String.fromCharCode(o.charCodeAt(0) ^ e))
+                }), i.join("""")
+            }
+        }
+     return o;
+}
+var reveal = res();";
         }
 
     }
